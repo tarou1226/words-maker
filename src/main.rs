af85d0dd54b880c6b8b9e4d8fb894c414e13dac0
taro <teroro1226@outlook.jp>
2022-12-09 02:54:39 +0900

    add command func

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::io::{ stdin, stdout, Write };
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut buf = String::new();
+        print!("Input command → ");
+        stdout().flush().unwrap();
+        stdin().read_line(&mut buf).unwrap();
+        match buf.trim() {
+            "create" => create_file(),
+            "add" => add_contents(),
+            "delete" => delete_file(),
+            "finished" => finished(),
+            _ => help(),
+        }
+    }
+}
+
+fn help() {
+    println!("help")
+}
+
+fn finished() {
+    println!("finished")
+}
+
+fn delete_file() {
+    println!("delete")
+}
+
+fn add_contents() {
+    println!("add")
+}
+
+fn create_file() {
+    println!("create")
 }
