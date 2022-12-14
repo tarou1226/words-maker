eb5db3e9ae71dd872ab9dcf77271d042a8039798
taro <teroro1226@outlook.jp>
2022-12-14 09:53:31 +0900

    add add_contents func

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io::{ stdin, stdout, Write };
-use std::fs::{File, self};
+use std::io::{ stdin, stdout, Write};
+use std::fs::{File, self, OpenOptions};
 use std::process;
 
 fn main() {
@@ -54,7 +54,34 @@ fn delete_file() {
 }
 
 fn add_contents() {
-    println!("add")
+    println!("add");
+    let mut buf = String::new();
+    print!("Input add file name → ");
+    stdout().flush().unwrap();
+    stdin().read_line(&mut buf).unwrap();
+
+    let file_name = String::from("./words/") 
+                            + buf.trim() 
+                            + &String::from(".txt");
+    // contentsに指定したファイルの内容を全て読み込む
+    let contents = fs::read_to_string(&file_name).unwrap();
+    println!("{}", contents);
+    
+    // 追加したい内容を記述する
+    let mut additional_contents = String::new();
+    print!("Input add contents → ");
+    stdout().flush().unwrap();
+    stdin().read_line(&mut additional_contents).unwrap();
+
+    // ファイルを読みこみ、contentsとadditional_contentsをbyte型に変換し、挿入する
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(file_name)
+        .unwrap();
+    
+    file.write_all((contents + "\n" + &additional_contents).as_bytes()).unwrap();
 }
 
 fn create_file() {
